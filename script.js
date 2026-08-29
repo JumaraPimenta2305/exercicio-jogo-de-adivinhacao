224757fa327f2220482a3fef628945532397053e
Jumara Pimenta <jumara.pimenta@hotmail.com>
2026-08-29 15:35:08 -0300

    Melhora jogo de adivinhacao

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const numeroSecreto = Math.floor(Math.random() * 100) + 1
+let numeroSecreto = Math.floor(Math.random() * 100) + 1
 const obterElemento = (id) => document.getElementById(id)
 
 const mensagem = obterElemento("mensagemResultado")
 const elementoTentativas = obterElemento("numeroTentativa")
 
 const tentativasMaximas = 10
-let tentativasRestantes = 10
+let tentativasRestantes = tentativasMaximas
+let jogoAtivo = true
 
 function chutar(){
-    
+     if(!jogoAtivo){
+        return
+     }
     const palpite = parseInt(obterElemento("inputPalpite").value)
 
     if(palpite < 1 || palpite > 100){
@@ -18,6 +21,7 @@ function chutar(){
 
     if(palpite === numeroSecreto){
         mensagem.textContent = "Você acertou!"
+        jogoAtivo = false
         return
     }else if(palpite > numeroSecreto){
         mensagem.textContent = "O número secreto é menor!"
@@ -30,6 +34,21 @@ function chutar(){
     
     if(tentativasRestantes === 0){
         mensagem.textContent = `Você perdeu! O número era ${numeroSecreto}`
+        jogoAtivo = false
         return
     }
+}
+function novoJogo() {
+
+    numeroSecreto = Math.floor(Math.random() * 100) + 1
+
+    tentativasRestantes = tentativasMaximas
+
+    jogoAtivo = true
+
+     mensagem.textContent = ""
+
+    elementoTentativas.textContent = `Tentativas: ${tentativasRestantes}`
+
+     obterElemento("inputPalpite").value = ""
 }
